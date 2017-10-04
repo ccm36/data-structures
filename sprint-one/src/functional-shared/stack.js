354cf7c87b8df49beff512867b09ff9b2b3e1740
ccm36 <cameron.c.malloy@gmail.com>
2017-10-04 16:00:19 -0500

    Add stack instance and ability to extend methods to instance

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage: {},
+    index: 0
+  };
+
+  extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
 var stackMethods = {};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
+stackMethods.size = function() {
+  if (this.index < 0) {
+    return 0;
+  }
+  return this.index;
+};
 
 
