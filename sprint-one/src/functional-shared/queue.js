1a0cc2ad4afc2dd85ab19a693887b0826b6cf14b
ccm36 <cameron.c.malloy@gmail.com>
2017-10-04 16:25:05 -0500

    Extend queue to have size functionality to report index

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,27 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage: {},
+    index: 0
+  };
+
+  extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
 var queueMethods = {};
+
+var extend = function(to, from) {
+  for(var key in from) {
+    to[key] = from[key];
+  }
+}
+
+queueMethods.size = function() {
+  if (this.index < 0) {
+    return this.index = 0;
+  }
+  return this.index;
+};
 
 
