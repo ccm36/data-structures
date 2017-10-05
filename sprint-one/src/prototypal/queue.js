358c2b692b0fc7d775440228c469a7246ea7412e
ccm36 <cameron.c.malloy@gmail.com>
2017-10-04 19:25:35 -0500

    Add functionality to insert & remove values from queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -9,6 +9,19 @@ var Queue = function() {
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.index] = value;
+  this.index++;
+};
+
+queueMethods.dequeue = function() {
+  var dequeued = this.storage[0];
+  delete this.storage[0];
+  this.storage[0] = this.storage[1];
+  this.index--;
+  return dequeued;
+};
+
 queueMethods.size = function() {
   if (this.index < 0) {
     return this.index = 0;
