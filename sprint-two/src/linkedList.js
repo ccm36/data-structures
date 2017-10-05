d7a1cc16731f59813fab4a6b236c6f2bf46037ae
ccm36 <cameron.c.malloy@gmail.com>
2017-10-05 14:49:57 -0500

    Add a function to append a node to the tail

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,8 +2,21 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list._size = 0;
+
 
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+    var currentNode = this.head;
+
+    if(!currentNode) {
+      this.head = newNode;
+      this.tail = newNode;
+      this._size++;
+    }
+
+    this.tail.next = newNode;
+    this.tail = newNode; 
   };
 
   list.removeHead = function() {
