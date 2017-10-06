592208ec043a7086ac4f7aca241a4258228f76ef
ccm36 <cameron.c.malloy@gmail.com>
2017-10-06 15:00:38 -0500

    Add pomander verification

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -10,7 +10,7 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  var child = new Tree(value)
+  var child = new Tree(value);
   this.children.push(child);
 };
 
