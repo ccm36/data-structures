03207fcb3008d4016b2303aed544a7773a0b631a
ccm36 <cameron.c.malloy@gmail.com>
2017-10-06 17:51:26 -0500

    Utilize LimitedArray construction to build HashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,17 +7,26 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[index] = v;
+  
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, {});
+  } 
+  
+  var bucket = this._storage.get(index);
+  bucket[k] = v;
+  
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage[index];
+  
+  return this._storage.get(index)[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  delete this._storage[index];
+  
+  delete this._storage.get(index)[k];
 };
 
 
