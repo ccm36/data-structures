83b5a7acf3a22eeab0113ac5ce4b21a353919c7c
ccm36 <cameron.c.malloy@gmail.com>
2017-10-06 15:32:45 -0500

    Add .insert/.retrieve/.remove functionality
    Need to work on method to handle collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,24 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[index] = v;
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage[index];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  delete this._storage[index];
 };
 
 
 
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: What is the time complexity of the
+ above functions?
  */
 
 
