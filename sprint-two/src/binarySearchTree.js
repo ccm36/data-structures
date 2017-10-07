67c422ddf1515036261237a9398337bb3716970b
ccm36 <cameron.c.malloy@gmail.com>
2017-10-07 11:47:17 -0500

    Add .insert & .contains functionality

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,23 +1,38 @@
 var BinarySearchTree = function(value) {
   this.value = value;
   
-  this.left;
-  this.right;
+  this.left = null;
+  this.right = null;
 };
 
 BinarySearchTree.prototype.insert = function(value) {
-
+  var newNode = new BinarySearchTree(value);
+  
+  if (value > this.value) {
+    this.right === null ? this.right = newNode : this.right.insert(value);
+  } else {
+    this.left === null ? this.left = newNode : this.left.insert(value);
+  }
 };
 
-BinarySearchTree.prototype.contains = function(value) {
+BinarySearchTree.prototype.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
   
+  if (this.right !== null && target > this.value) {
+    return this.right.contains(target);
+  } else if (this.left !== null) {
+    return this.right.contains(target);
+  }
+  
+  return false;
 };
 
 BinarySearchTree.prototype.depthFirstLog = function(cb) {
   
 };
 
-// var myBST = new BinarySearchTree(6);
 
 
 /*
