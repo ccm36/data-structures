39071f668bf906a0a22b4ae9cc936f644e0b1354
ccm36 <cameron.c.malloy@gmail.com>
2017-10-07 09:46:23 -0500

    Create basic function skeletons

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,23 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  
+  this.left;
+  this.right;
 };
+
+BinarySearchTree.prototype.insert = function(value) {
+
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+  
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  
+};
+
+// var myBST = new BinarySearchTree(6);
 
 
 /*
